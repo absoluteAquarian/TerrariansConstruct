9bae0719e1daf540b8bb80d47ef6f05bd009631b
absoluteAquarian <jruss88@me.com>
2022-03-13 16:36:25 -0500

    library update + parts for Unloaded and Unkown materials

--- src/CoreMod.Parts.cs
+++ src/CoreMod.Parts.cs
@@ -1,6 +1,8 @@
 ﻿using Terraria.ID;
 using TerrariansConstructLib;
 using TerrariansConstructLib.ID;
+using TerrariansConstructLib.Materials;
+using TerrariansConstructLib.Registry;
 
 namespace TerrariansConstruct {
 	partial class CoreMod {
@@ -11,6 +13,9 @@ namespace TerrariansConstruct {
 			CoreLibMod.AddAllPartsOfType(this, ItemID.Wood, ItemRarityID.White, TCPartActions.Wood, "[c/996633:Wimpy I]", MaterialPartID.WeaponBowString);
 
 			CoreLibMod.AddPart(this, ItemID.Cobweb, ItemRarityID.White, MaterialPartID.WeaponBowString, TCPartActions.Cobweb, null);
+			
+			CoreLibMod.AddAllPartsOfMaterial(this, new UnloadedMaterial(), PartActions.NoActions, "[c/fc51ff:Unloaded Part]");
+			CoreLibMod.AddAllPartsOfMaterial(this, new UnknownMaterial(), PartActions.NoActions, "[c/616161:Unknown Part]");
 
 			CoreLibMod.LogAddedParts = false;
 		}
