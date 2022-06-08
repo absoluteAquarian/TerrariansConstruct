4309fc4cf9d419cda532be4696d1f297d40efac1
absoluteAquarian <jruss88@me.com>
2022-06-08 15:20:01 -0500

    made GoldTrait trigger even at 0 luck

--- src/Modifiers/Traits/GoldTrait.cs
+++ src/Modifiers/Traits/GoldTrait.cs
@@ -29,7 +29,7 @@ namespace TerrariansConstruct.Modifiers.Traits {
 			// - a 1/200 chance (affected by luck) must succeed
 			// - the tile wasn't placed by a player
 			// - ore tile type must have a registered item type
-			if (player.luck > 0 && TileID.Sets.Ore[context.type] && player.RollLuck(200) == 0 && !OrePlacementTracker.tilesPlacedByPlayers.Contains(new(x, y)) && CoreMod.oreTileToOreItem.TryGetValue(context.type, out int itemType)) {
+			if (player.luck >= 0 && TileID.Sets.Ore[context.type] && player.RollLuck(200) == 0 && !OrePlacementTracker.tilesPlacedByPlayers.Contains(new(x, y)) && CoreMod.oreTileToOreItem.TryGetValue(context.type, out int itemType)) {
 				//Spawn another item
 				Item.NewItem(new EntitySource_TileBreak(x, y), x * 16, y * 16, 16, 16, itemType);
 
