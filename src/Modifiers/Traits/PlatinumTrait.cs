94e28e4955830cc34c3c85c5846b95c5353a87e7
absoluteAquarian <jruss88@me.com>
2022-06-07 19:40:05 -0500

    modified PlatinumTrait stats

--- src/Modifiers/Traits/PlatinumTrait.cs
+++ src/Modifiers/Traits/PlatinumTrait.cs
@@ -8,6 +8,8 @@ using TerrariansConstructLib.Projectiles;
 
 namespace TerrariansConstruct.Modifiers.Traits {
 	internal sealed class PlatinumTrait : BaseTrait {
+		public override bool IsSingleton => true;
+
 		public override Color TooltipColor => new(0x99, 0xb4, 0xcc);
 
 		public override string LangKey => "Mods.TerrariansConstruct.PartTooltips.Platinum";
@@ -38,18 +40,18 @@ namespace TerrariansConstruct.Modifiers.Traits {
 		private void ModifyNPCHitStats(ref int damage, ref float knockBack, bool atFullHealth) {
 			//+20% damage on full-health NPCs per tier, additive
 			//+10% knockback on full-health NPCs per tier, additive
-			ModifyHitStats(ref damage, ref knockBack, atFullHealth, 0.2f, 0.1f);
+			ModifyHitStats(ref damage, ref knockBack, atFullHealth, 0.5f, 0.1f);
 		}
 
 		private void ModifyPlayerHitStats(ref int damage, bool atFullHealth) {
 			//+10% damage on full-health players per tier, additive
 			float knockBack = 0;
-			ModifyHitStats(ref damage, ref knockBack, atFullHealth, 0.1f, 0f);
+			ModifyHitStats(ref damage, ref knockBack, atFullHealth, 0.3f, 0f);
 		}
 
 		private void ModifyHitStats(ref int damage, ref float knockBack, bool atFullHealth, float damageFactor, float knockBackFactor) {
 			StatModifier damageBonus = new(atFullHealth ? 1f + Tier * damageFactor : 1f, 1f);
-			damage = (int)damageBonus.ApplyTo(damage);
+			damage = (int)Math.Ceiling(damageBonus.ApplyTo(damage));
 
 			//Tier bonus capped at level 10
 			StatModifier kbBonus = new(atFullHealth ? 1f + Math.Max(10, Tier) * knockBackFactor : 1f, 1f);
